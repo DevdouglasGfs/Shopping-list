5e5d88880873877432148ab33401f07d358b2009
Douglas Silva <devdouglassilva@gmail.com>
2023-07-31 13:05:38 -0300

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ function ListProducts(product) {
   PRODUCTS_LIST.innerHTML = "";
 
   PRODUCTS.forEach(productOnProducts => {
-    const ID = product.id;
+    const ID = productOnProducts.id;
     const PRODUCT_FRAGMENT = document.createDocumentFragment();
     const LI = document.createElement("li");
     const PRODUCT = document.createElement("div");
@@ -54,12 +54,12 @@ function ListProducts(product) {
 
     PRODUCT.classList.add("shopping-list__product");
     PRODUCT.setAttribute("data-product-id", ID);
-    PRODUCT_NAME.textContent = `${product.name}`;
+    PRODUCT_NAME.textContent = `${productOnProducts.name}`;
     PRODUCT_DATA.classList.add("shopping-list__product-data");
     PRODUCT_DATA_DETAILS.classList.add("shopping-list__product-data-details")
-    PRODUCT_AMOUNT.textContent = `Quantidade: ${product.amount}`;
+    PRODUCT_AMOUNT.textContent = `Quantidade: ${productOnProducts.amount}`;
     PRODUCT_AMOUNT.classList.add("product-amount");
-    PRODUCT_PRICE.textContent = `Preço total: ${product.price * product.amount}`;
+    PRODUCT_PRICE.textContent = `Preço total: ${productOnProducts.price * productOnProducts.amount}`;
     PRODUCT_PRICE.classList.add("product-price");
     PRODUCT_CTAS.classList.add("shopping-list__product-ctas");
     PRODUCT_CTA_ADD.classList.add("add-product");
@@ -68,13 +68,13 @@ function ListProducts(product) {
     PRODUCT_CTA_ADD.textContent = "+";
     PRODUCT_CTA_ADD.addEventListener("click", AddProducts);
 
-    if (product.amount === 0) {
+    if (productOnProducts.amount === 0) {
       PRODUCT_CTA_REMOVE.classList.add("remove-product");
       PRODUCT_CTA_REMOVE.type = "button";
       PRODUCT_CTA_REMOVE.value = "-";
       PRODUCT_CTA_REMOVE.textContent = "-";
       PRODUCT_CTA_ADD.onclick = "RemoveProducts";
-    } else if (product.amount > 0) {
+    } else if (productOnProducts.amount > 0) {
       PRODUCT_CTA_REMOVE.classList.add("remove-product");
       PRODUCT_CTA_REMOVE.classList.add("removable");
       PRODUCT_CTA_REMOVE.type = "button";
@@ -98,8 +98,8 @@ function ListProducts(product) {
   })
 }
 /**
- * PRODUCTS =[{}]
- * e = event <— button
+ * PRODUCTS = [] (array vazio)
+ * e = evento <— botão
  * product = {id, name, price, amount} <— PRODUCTS[] <— ListProducts()
  * PRODUCT_LIST = <ul>...<ul/>
  * PRODUCT_LIST_ITEMS = MapNode <— [<li>...<li/>]
@@ -109,25 +109,24 @@ function AddProducts(e) {
   const PRODUCT_CTA_ADD = e.target;
   const PRODUCT_LIST = document.querySelector(".shopping-list__list");
   const PRODUCT_LIST_ITEMS = PRODUCT_LIST.childNodes;
-  let productOnItem;
 
   PRODUCT_LIST_ITEMS.forEach(item => {
-    productOnItem = item.firstChild;
-  })
-  let amount = Number(productOnItem.querySelector(".product-amount").textContent.split(" ")[1]);
+    let productOnItem = item.firstChild;
+    let amount = Number(productOnItem.querySelector(".product-amount").textContent.split(" ")[1]);
   
-  PRODUCTS.forEach(product => {
-    if (Number(productOnItem.getAttribute("data-product-id")) === product.id) {
-      amount += 1;
-      const NEW_PRODUCT_DATA = {
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        amount: amount
+    PRODUCTS.forEach(product => {
+      if (Number(productOnItem.getAttribute("data-product-id")) === product.id) {
+        amount += 1;
+        const NEW_PRODUCT_DATA = {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          amount: amount
+        }
+        UpdateElementValues(product, NEW_PRODUCT_DATA);
       }
-      UpdateElementValues(product, NEW_PRODUCT_DATA);
-    }
-  })
+    })
+  });
 }
 
 /**
@@ -145,29 +144,34 @@ function UpdateElementValues(product, productData) {
       console.log(product, productData)
 
       if (PRODUCT_LIST.contains(PRODUCT_ON_LIST)) {
-        const PRODUCT_NAME = PRODUCT_LIST.querySelector(".product-name");
-        const PRODUCT_PRICE = PRODUCT_LIST.querySelector(".product-price");
-        const PRODUCT_AMOUNT = PRODUCT_LIST.querySelector(".product-amount").textContent.split(" ")[1]
+        const PRODUCT_NAME = PRODUCT_ON_LIST.querySelector(".product-name");
+        const PRODUCT_PRICE = PRODUCT_ON_LIST.querySelector(".product-price");
+        const PRODUCT_AMOUNT = PRODUCT_ON_LIST.querySelector(".product-amount");
 
         if (NAME !== product.name || PRICE !== product.price || AMOUNT !== product.amount) {
-          let newData = {}
+          let newData = {};
           newData.id = ID;
           
           if (NAME !== product.name) {
             product.name = NAME;
             PRODUCT_NAME.textContent = NAME;
-            newData.name = NAME
+            newData.name = NAME;
           }
+          console.log(product.name)
           if (PRICE !== product.price) {
             product.price = PRICE;
             PRODUCT_PRICE.textContent = PRICE;
-            newData.name = NAME
+            newData.price = PRICE;
           }
+          console.log(product.price)
           if (AMOUNT !== product.amount) {
             product.amount = AMOUNT;
             PRODUCT_AMOUNT.textContent = AMOUNT;
-            newData.name = NAME
+            newData.amount = AMOUNT;
           }
+          console.log(product.amount)
+          // Atualizando o objeto do produto original com os novos dados
+          Object.assign(product, newData);
         }
         console.log("step 2")
         console.log(product, productData)
@@ -175,7 +179,3 @@ function UpdateElementValues(product, productData) {
     }
   }
 }
-
-/**
- * Adicionar ouvintes dr eventos para alterar os dados
- * */
